0436ca74d4ab890170051000cd69e80e58e6c43e
teddywallen <89647911+teddywallen@users.noreply.github.com>
2021-09-01 11:06:57 -0400

    Add files via upload

---
+++ First.java
@@ -0,0 +1,8 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        for(String word : args) {
+            System.out.println(word);
+        }
+    }
+}
